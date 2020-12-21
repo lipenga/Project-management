98aa5d17f28fe087b18f574c8f8484dbad1186f2
“李鹏29306” <654488010@qq.com>
2020-12-21 10:54:23 +0800

    “null”

--- src/pages/dashboard/analysis/index.tsx
+++ src/pages/dashboard/analysis/index.tsx
@@ -1,7 +1,7 @@
 // Analyasis
 import React, { useState, useRef, useEffect, useCallback, useContext } from 'react'
 import { test } from './service'
-
+import { Table, Tag, Space } from 'antd';
 /** 
  * Preset 
  */
@@ -31,14 +31,61 @@ const Analyasis: React.FC<{}> = () => {
   /** 
    * componentsConfig 
    */
+  const columns = [
+    {
+      title: '姓名',
+      dataIndex: 'name',
+      key: 'name',
 
+    },
+    {
+      title: '邮箱',
+      dataIndex: 'age',
+      key: 'age',
+    },
+    {
+      title: '角色',
+      dataIndex: 'address',
+      key: 'address',
+    },
+    {
+      title: '操作',
+      key: 'tags',
+      dataIndex: 'tags',
 
+    },
+
+  ];
+  const data = [
+    {
+      key: '1',
+      name: 'John Brown',
+      age: 32,
+      address: 'New York No. 1 Lake Park',
+      tags: ['nice', 'developer'],
+    },
+    {
+      key: '2',
+      name: 'Jim Green',
+      age: 42,
+      address: 'London No. 1 Lake Park',
+      tags: ['loser'],
+    },
+    {
+      key: '3',
+      name: 'Joe Black',
+      age: 32,
+      address: 'Sidney No. 1 Lake Park',
+      tags: ['cool', 'teacher'],
+    },
+  ];
   /** 
    * render
    */
   return (
     <>
-      <h1>欢迎来到首页....</h1>
+
+      <Table columns={columns} bordered dataSource={data} />
     </>
   )
 }
